b7c7d404c4d3a1c5014db2a890ad06d813bdd464
RamK03 <ram0436403@gmail.com>
2025-04-02 15:03:50 +0530

    random class

---
+++ src/BasicProblems/src/RandomClass.java
@@ -0,0 +1,12 @@
+package BasicProblems.src;
+import java.util.*;
+public class RandomClass
+{
+    public static void main(String[] args)
+    {
+        Random d=new Random();
+        int j=(int)(Math.random()*1000000);
+        System.out.println(j);
+
+    }
+}
